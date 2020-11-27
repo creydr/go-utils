73316e8c79b2d9d2c11be795baf9688b7af12a1b
creydr <chresse1605@gmail.com>
2020-11-27 21:02:45 +0100

    Addes some more tests for slice.IsEquals* functions

--- slice/equals_test.go
+++ slice/equals_test.go
@@ -2,6 +2,7 @@ package slice
 
 import (
 	"fmt"
+	"testing"
 )
 
 func ExampleIsEqualStringSlice() {
@@ -36,4 +37,62 @@ func ExampleIsEqualIntSlice() {
 	// true
 	// false
 	// true
+}
+
+func TestIsEqualStringSlice(t *testing.T) {
+	tests := []struct {
+		name string
+		s1   []string
+		s2   []string
+		want bool
+	}{
+		{
+			name: "Should return false on slices with different length",
+			s1:   []string{"foo", "bar", "baz"},
+			s2:   []string{"foo", "baz"},
+			want: false,
+		},
+		{
+			name: "Should return false on slices with same length but different values",
+			s1:   []string{"foo", "bar", "baz"},
+			s2:   []string{"foo", "baz", "foobar"},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsEqualStringSlice(tt.s1, tt.s2); got != tt.want {
+				t.Errorf("IsEqualStringSlice() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsEqualIntSlice(t *testing.T) {
+	tests := []struct {
+		name string
+		s1   []int
+		s2   []int
+		want bool
+	}{
+		{
+			name: "Should return false on slices with different length",
+			s1:   []int{1, 2, 3},
+			s2:   []int{1, 2},
+			want: false,
+		},
+		{
+			name: "Should return false on slices with same length but different values",
+			s1:   []int{1, 2, 3},
+			s2:   []int{1, 3, 2},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsEqualIntSlice(tt.s1, tt.s2); got != tt.want {
+				t.Errorf("IsEqualIntSlice() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
